d474fd46ab9bbc188ca7ee9d71c69662b52748a8
Naomi Dax <imkadax@gmail.com>
2024-07-09 12:53:54 +0000

    feature: add external link when clicking knowledge button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
 //adaptation of https://www.w3schools.com/howto/howto_css_modals.asp
-// Get the modal
+// Get the modal & button that opens the modal
 var modal = document.getElementById("how-to-play-container");
-
-// Get the button that opens the modal
 var btn = document.getElementById("how-to-play-btn");
-
-
 // When the user clicks on the button, open the modal
+//kann ich hier nicht einfach so ähnlich wi unten machen mit if id=="" then?
 btn.onclick = function() {
   modal.style.display = "flex";
   modal.style.justifyContent = "center";
   modal.style.alignItems = "center";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+//open link in new tab when clicking knowledge button
+function openLink() {
+
+  window.open("https://bioshock.fandom.com/wiki/BioShock_Wiki");
+
 }
