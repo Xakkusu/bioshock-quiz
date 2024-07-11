ddcc7c5d10d3ee1de6b81697b3d0beef1f25ac7a
Naomi Dax <imkadax@gmail.com>
2024-07-11 13:29:13 +0000

    add basic javascript function and variable structure for the quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,6 +2,9 @@
 // Get the modal & button that opens the modal
 let modal = document.getElementById("container-game-area");
 let btn = document.getElementsByClassName("game-to-choose");
+
+
+
 // When the user clicks on the button, open the modal
 //kann ich hier nicht einfach so ähnlich wi unten machen mit if id=="" then?
 for (let i = 0; i<btn.length; i++){
@@ -17,4 +20,70 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   } 
+}
+
+
+// used https://www.youtube.com/watch?v=PBcqGxrr9g8&t=14s to get to know the basic structure
+// of how to implement content for questions
+// and load it to the website, then working on my own on it
+
+// variables for the quiz
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+const next = document.getElementById("next-btn");
+//score will be 0 at the start of the game
+let score = 0;
+// number of questions left
+let numberOfQuestions = 15;
+
+
+// content for questions and their answers
+const questions = [
+    {
+        question : "blablabla1?",
+        answers : [
+            { text : "a", correct : false},
+            { text : "b", correct : false},
+            { text : "c", correct : true},
+            { text : "d", correct : false}
+        ]
+    },
+    {
+        question : "blablabla2?",
+        answers : [
+            { text : "a", correct : false},
+            { text : "b", correct : true},
+            { text : "c", correct : false},
+            { text : "d", correct : false}
+        ]
+    },
+    {
+        question : "blablabla3?",
+        answers : [
+            { text : "a", correct : true},
+            { text : "b", correct : false},
+            { text : "c", correct : false},
+            { text : "d", correct : false}
+        ]
+    }
+];
+
+// function to start the game
+function runGame () {
+
+}
+
+// function to increase value of score when answering correct
+function incrementScore () {
+
+}
+
+// function to decrease how many questions are left
+function questionsLeft () {
+
+}
+
+// function to show timer of 20 seconds
+function runTimer () {
+    
 }
