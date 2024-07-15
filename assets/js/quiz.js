0b52e3d0f5a85c4a0c6c4f5890497199cc4f8330
Naomi Dax <imkadax@gmail.com>
2024-07-15 13:16:00 +0000

    add objects for other question-topics

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -74,6 +74,66 @@ const questions = [
     }
 ];
 
+const questionsB2 = [
+    {
+        question : "blablabla1?",
+        answers : [
+            { text : "a", correct : false},
+            { text : "b", correct : false},
+            { text : "c", correct : true},
+            { text : "d", correct : false}
+        ]
+    },
+    {
+        question : "blablabla2?",
+        answers : [
+            { text : "a", correct : false},
+            { text : "b", correct : true},
+            { text : "c", correct : false},
+            { text : "d", correct : false}
+        ]
+    },
+    {
+        question : "blablabla3?",
+        answers : [
+            { text : "a", correct : true},
+            { text : "b", correct : false},
+            { text : "c", correct : false},
+            { text : "d", correct : false}
+        ]
+    }
+];
+
+const questionsB3 = [
+    {
+        question : "blablabla1?",
+        answers : [
+            { text : "a", correct : false},
+            { text : "b", correct : false},
+            { text : "c", correct : true},
+            { text : "d", correct : false}
+        ]
+    },
+    {
+        question : "blablabla2?",
+        answers : [
+            { text : "a", correct : false},
+            { text : "b", correct : true},
+            { text : "c", correct : false},
+            { text : "d", correct : false}
+        ]
+    },
+    {
+        question : "blablabla3?",
+        answers : [
+            { text : "a", correct : true},
+            { text : "b", correct : false},
+            { text : "c", correct : false},
+            { text : "d", correct : false}
+        ]
+    }
+];
+
 // function to set up game
 function setUp(){
     questionsAsked = 0; 
