fa0f54721d6e3dcaf3a58c8d664e95021c9287a3
Naomi Dax <imkadax@gmail.com>
2024-07-20 09:18:07 +0000

    add first question set to questions file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -26,6 +26,114 @@ const questionsB1 = [
             { text : "Family reunion", correct : false},
             { text : "Car crash", correct : false}
         ]
+    },
+    {
+        question : "Who is the founder of Rapture?",
+        answers : [
+            { text : "Andrew Ryan", correct : true},
+            { text : "Atlas", correct : false},
+            { text : "Brigid Tenenbaum", correct : false},
+            { text : "Frank Fontaine", correct : false}
+        ]
+    },
+    {
+        question : "How are the most common enemies in the game called",
+        answers : [
+            { text : "Soldiers", correct : false},
+            { text : "Capitalists", correct : false},
+            { text : "Robots", correct : false},
+            { text : "Splicer", correct : true}
+        ]
+    },
+    {
+        question : "What grants the player super-human powers?",
+        answers : [
+            { text : "The bible", correct : false},
+            { text : "Plasmids", correct : true},
+            { text : "Hard work", correct : false},
+            { text : "Guns", correct : false}
+        ]
+    },
+    {
+        question : "What substance does one need to use plasmids?",
+        answers : [
+            { text : "Solomon", correct : false},
+            { text : "Eve", correct : false},
+            { text : "Adam", correct : true},
+            { text : "David", correct : false}
+        ]
+    },
+    {
+        question : "From whom can you harvest Adam?",
+        answers : [
+            { text : "Big Sisters", correct : false},
+            { text : "Little Sisters", correct : true},
+            { text : "Little Mama", correct : false},
+            { text : "Little Cousin", correct : false}
+        ]
+    },
+    {
+        question : "Does saving or harvesting the little sisters grant more Adam at the end of the game?",
+        answers : [
+            { text : "Both are the same", correct : false},
+            { text : "Savivng", correct : false},
+            { text : "Harvesting", correct : true},
+            { text : "Pure luck", correct : false}
+        ]
+    },
+    {
+        question : "Who should you save for the revolutionary Atlas?",
+        answers : [
+            { text : "Himself", correct : false},
+            { text : "Rapture", correct : false},
+            { text : "Andrew Ryan", correct : false},
+            { text : "His family", correct : true}
+        ]
+    },
+    {
+        question : "When killing Andrew Ryan for Atlas what is the secret being revealed?",
+        answers : [
+            { text : "The little sisters are your children", correct : false},
+            { text : "Andrew Ryan is your father", correct : true},
+            { text : "Atlas is your father", correct : false},
+            { text : "Brigid Tenenbaum is your mother", correct : false}
+        ]
+    },
+    {
+        question : "What is Atlas' real identity?",
+        answers : [
+            { text : "Frank Fontain", correct : true},
+            { text : "Sander Cohen", correct : false},
+            { text : "Zachary Comstock", correct : false},
+            { text : "J.S. Steinman", correct : false}
+        ]
+    },
+    {
+        question : "With which sentence can Jack be controlled??",
+        answers : [
+            { text : "... do for me swiftly", correct : false},
+            { text : "Be a good boy and ...", correct : false},
+            { text : "Could you please?", correct : false},
+            { text : "Would you kindly?", correct : true}
+        ]
+    },
+    {
+        question : "How does Atlas come up with the name of his 'family', Moira and Patrick?",
+        answers : [
+            { text : "These are his real family names", correct : false},
+            { text : "They were employees in a pub", correct : false},
+            { text : "A Stage play by Sander Cohen", correct : true},
+            { text : "These are Andrew Ryans parents", correct : false}
+        ]
+    },
+    {
+        question : "How many endings are there to Bioshock?",
+        answers : [
+            { text : "Three", correct : true},
+            { text : "Four", correct : false},
+            { text : "Two", correct : false},
+            { text : "One", correct : false}
+        ]
     }
 ];
 
