a488ecd61c2804483ade1d1118e42b48c4f10aa6
Naomi Dax <imkadax@gmail.com>
2024-07-25 09:18:47 +0000

    refactor: Change message shown when pressing let's play button

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,7 +69,7 @@ letsPlayBtn.addEventListener("click", function(event){
   } else {
     event.preventDefault();
     localStorage.setItem("userName", namePlayer.value);
-    alert(`${localStorage.getItem("userName")} lez go`);
+    alert(`Hi, ${localStorage.getItem("userName")}, let the game begin`);
     window.location.replace("quiz.html");
   }
 });
